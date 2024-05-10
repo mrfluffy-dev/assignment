d61322db35f5e4b6b89b52b3c0e69a09360a486c
Zastian Pretorius <zastian00@gmail.com>
2024-05-10 12:27:42 +0100

    fromt end changes

--- public/pages/people.js
+++ public/pages/people.js
@@ -1,8 +1,8 @@
 async function loadPeople(name,id) {
     try {
         // fix name from spaces to dashes
-        name = name.replace(' ', '+');
-        const response = await fetch(`http://localhost:4200/api/${name}`);
+        name = name.replace(/ /g, '+');
+        const response = await fetch(`https://wars.mrfluffy.xyz/api/${name}`);
         const data = await response.json();
         const peopleElement = document.getElementById('people'+id);
         // Clear any existing content
@@ -22,5 +22,27 @@ async function loadPeople(name,id) {
         peopleElement.appendChild(personElement);
     } catch (error) {
         console.error('Error loading people:', error);
+    }
+}
+
+// make a function to load the people it takes in to parameters name and name2 and and calles http://localhost:4200/battle/name/name2 plave the responce in id battle
+async function loadBattle(name, name2) {
+    try {
+        // fix name from spaces to dashes
+        name = name.replace(/ /g, '+');
+        name2 = name2.replace(/ /g, '+');
+        const response = await fetch(`https://wars.mrfluffy.xyz/battle/${name}/${name2}`);
+        let data = await response.text(); // Assuming the response is plain text
+        const battleElement = document.getElementById('battle');
+        // Clear any existing content
+        battleElement.innerHTML = '';
+        // Create a div element for the battle
+        const battleDiv = document.createElement('div');
+        // Replace all \n with <br> and set the text content using the battle story
+        battleDiv.innerHTML = data.replace(/\\n/g, '<br>');
+        // Append the battleDiv element to the battleElement
+        battleElement.appendChild(battleDiv);
+    } catch (error) {
+        console.error('Error loading battle:', error);
     }
 }
